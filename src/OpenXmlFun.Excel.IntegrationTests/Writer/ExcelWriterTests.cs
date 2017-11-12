fe57b8a94d390fdd6de2fdf9284eda8e3d6c2576
Maxim Tkachenko <oblomov86@gmail.com>
2017-11-12 18:54:23 +0300

    added empty ExcelWriterTests

---
+++ src/OpenXmlFun.Excel.IntegrationTests/Writer/ExcelWriterTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace OpenXmlFun.Excel.IntegrationTests.Writer
+{
+    [TestFixture]
+    class ExcelWriterTests
+    {
+    }
+}
